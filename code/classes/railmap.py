e869bb689ab85682139347995ddb7f81d352f8f4
Isa Wagemans <i.wagemans@student.vu.nl>
2025-01-15 13:40:00 +0100

    quality K p erin gezet

--- code/classes/railmap.py
+++ code/classes/railmap.py
@@ -46,18 +46,18 @@ class Railmap():
         self.connections = connections
 
     def add_trajectory(self, train):
+        """"Route aanroepen"""
         self.trajectories[train.id] = train
 
-    def fraction_p(self):
-        "Compute fraction p of used connections"
-        self.p = 0.8 # example value -> needs to be computed!
 
     def quality_K(self):
-        T = len(self.trajectories) 
+        #Compute fraction p of used connections
+        self.p = 0.8 # example value -> needs to be computed!
+        T = len(self.trajectories)
         Min = 0
 
         for values in self.trajectories.values():
             Min += values[1]
-        
+
         # compute quality of the lines K
         return (self.p * 10000 - (T * 100 + Min))
