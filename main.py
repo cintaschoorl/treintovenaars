5e9e4d41ba9b2264ff951dce3031b76560236d25
cintaschoorl <schoorlcinta@gmail.com>
2025-01-16 12:56:18 +0100

    main print random station attributes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
+import random
 from code.classes.route import Route
 from code.classes.railmap import Railmap
-from code.classes.connection import Connection
 
 
 
@@ -17,23 +16,6 @@ if __name__ == "__main__":
     railsystem.load_stations(stations_path, uid_path, connections_path)
     print(railsystem.stations)
 
-
-    station1 = railsystem.stations["Amsterdam Centraal"]
-    station2 = railsystem.stations["Amsterdam Sloterdijk"]
-    connection1 = Connection(station1, station2, 15)
-
-    station3 = railsystem.stations["Utrecht Centraal"]
-    station4 = railsystem.stations["Amersfoort"]
-    connection2 = Connection(station3, station4, 20)
-
-    train_connections = [connection1, connection2]
-
-    train = ('train001', train_connections)
-
-    railsystem.add_trajectory(train)
-
-    #railsystem.add_trajectory(railsystem.trajectories)
-    print(railsystem.trajectories)
-
-    # print(type(Railmap.quality_K))
-    # print(Railmap.quality_K)
+    # test run to print the attributes of a random station
+    random_station = random.choice(railsystem.stations)
+    print(f"Name: {random_station.name}\nCoordinates: {random_station.coordinates}\nNeighbours: {random_station.neighbours}")
