0babcfbad1e1979a9d14553691568558207b0b67
Isa Wagemans <i.wagemans@student.vu.nl>
2025-01-16 13:18:26 +0100

    klein algoritme

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -1,1 +1,11 @@
-
+class Randomise():
+    spent_time = 50
+    start_station = 'slo'
+    route = Route(stations, max_duration=120)
+    route.add_station(start_station)
+    next_station, travel_time = route.get_neighbours(start_station)
+    spent_time += travel_time
+    if route.is_valid(spent_time) == True:
+        whole_route = route.add_station(next_station)
+    start_station = next_station
+    print(whole_route)
