e083cf3ce612dd6c590ab2215b4c2427b405af9d
Isa Wagemans <i.wagemans@student.vu.nl>
2025-01-20 11:10:17 +0100

    de route niet heen en weer van stations

--- code/algorithms/randomise.py
+++ code/algorithms/randomise.py
@@ -15,21 +15,31 @@ def randomise_route(stations_list, max_duration=120):
     route.add_station(current_station)
     print(f"Starting route at: {current_station.name}")
 
+    previous_station = None
+
     while spent_time <= max_duration:
+
         # get current neighbours
         neighbours = route.get_neighbours(current_station)
 
+        # get the neighbours that are not the previous stations
+        valid_neighbours = {}
+        for station, time in neighbours.items():
+            if station != previous_station:
+                valid_neighbours[station] = time
+
         # get random neighbour and travel time
-        next_station, next_time =  random.choice(list(neighbours.items()))
+        next_station, next_time =  random.choice(list(valid_neighbours.items()))
 
         # validate if it will be longer than max duration
         if route.is_valid((spent_time + next_time)):
             # update route and time with the new connection
-            whole_route = route.add_station(next_station)
+            route.add_station(next_station)
+            previous_station = current_station
             current_station = next_station
             spent_time += next_time
             print(f"{next_station} added to route, travel time: {next_time}.\nTotal route duration: {spent_time}.")
         else:
             break
 
-    return whole_route, spent_time
+    return route.route, spent_time
