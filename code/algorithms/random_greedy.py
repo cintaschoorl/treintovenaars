bc3003c0aa59e2e00bdd7afd5d119b08159aa77d
Emma van Tuinen <emmavantuinen@ip-145-137-137-253.wlan-int.ru.nl>
2025-01-28 11:47:10 +0100

    adding the heuristic of a starting station to random greedy

--- code/algorithms/random_greedy.py
+++ code/algorithms/random_greedy.py
@@ -25,7 +25,14 @@ def greedy_route(railmap, max_duration):
     """
 
     global used_connections
-    current_station = random.choice(railmap.stations)
+
+    starting_stations = [station for station in railmap.stations if len(station.neighbours) == 1]
+
+    if starting_stations:
+        current_station = random.choice(starting_stations)
+    else:
+        current_station = random.choice(railmap.stations)
+
 
     route = Route(railmap.stations, max_duration)
     route.add_station(current_station)
@@ -105,28 +112,27 @@ def random_greedy_algorithm(stations_path, uid_path, connections_path, num_route
     railmap = Railmap()
     railmap.load_stations(stations_path, uid_path, connections_path)
 
-    # Open the CSV file for appending results
+
     with open(output_csv, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
 
-        # Write the score for each iteration
+        # Writing down the score for each iteration
         for iteration in range(iterations):
             reset_connections(railmap)
 
-            # Reset the railmap for each iteration and run the routes
+            # Resetting the railmap for each iteration and running the routes
             for i in range(num_routes):
                 route = greedy_route(railmap, max_duration)
                 route.id = f"train_{i + 1}"
                 railmap.add_trajectory(route)
 
-            # Calculate the quality score after running the routes
+            # Calculating the quality score
             quality_score = railmap.quality_K()
 
-            # Write the iteration and quality score into the CSV file
+            # Writing into the CSV file
             writer.writerow([iteration + 1, quality_score])
 
     return railmap.quality_K()  # Return final quality score
-
 
 
 
