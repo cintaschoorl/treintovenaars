aad262454c7dc6114a9dd969ebd47ba5bc6d6ed2
cintaschoorl <schoorlcinta@gmail.com>
2025-01-29 16:33:32 +0100

    statistics plots naar juiste folders

--- code/visualization/statistics.py
+++ code/visualization/statistics.py
@@ -9,6 +9,7 @@ def plot_quality_score_histogram(json_file: json, algorithm: str, region: str):
     Input:
         - json_file (json): path to the JSON file containing quality scores
         - algorithm (str): name of the algorithm (for title and file naming)
+        - region (str): name of the region (Holland or Netherlands) for correct saving
     """
     # load JSON data
     with open(json_file, 'r') as file:
@@ -49,6 +50,7 @@ def plot_iteration_scores(json_file: json, algorithm: str, region: str):
     Input:
         - json_file (json): path to the JSON file containing iteration scores.
         - algorithm (str): name of the algorithm (for title and file naming).
+        - region (str): name of the region (Holland or Netherlands) for correct saving
     """
     # load JSON data
     with open(json_file, 'r') as file:
@@ -85,108 +87,3 @@ def plot_iteration_scores(json_file: json, algorithm: str, region: str):
         
     # show plot
     plt.show()
-
-if __name__ == "__main__":
-    plot_quality_score_histogram("output/best_railmap_random.json", "random")
-    plot_quality_score_histogram("output/best_railmap_greedy.json", "greedy")
-    plot_iteration_scores("output/best_railmap_hillclimber.json", "hillclimber")
-    plot_iteration_scores("output/best_railmap_sim_ann.json", "sim_ann")
-
-
-# # Visualize Random
-# def plot_random(input_file):
-#     output_path = '../../output/quality_scores_histogram.png'
-#     # Lees de CSV file
-#     df = pd.read_csv(input_file)
-
-#     # Maak het histogram
-#     plt.figure(figsize=(10, 6))
-#     plt.hist(df['quality_score'], bins=20, edgecolor='black')
-#     plt.title('Distribution of Quality Scores (K) for Random Algorithm')
-#     plt.xlabel('Quality Score (K)')
-#     plt.ylabel('Frequency')
-
-#     # Voeg gemiddelde en standaarddeviatie toe aan de plot
-#     mean = df['quality_score'].mean()
-#     std = df['quality_score'].std()
-#     plt.axvline(mean, color='red', linestyle='dashed', linewidth=1)
-#     plt.text(mean*1.1, plt.ylim()[1]*0.9, f'Mean: {mean:.2f}\nStd: {std:.2f}')
-
-#     # Sla de plot op
-#     plt.savefig('output/quality_scores_histogram.png')
-#     plt.show()
-
-
-# # visualize random greedy
-# def plot_random_greedy(input_file):
-#     """
-#     Visualizing the distribution of quality scores from the random greedy algorithm.
-#     """
-#     df = pd.read_csv(input_file)
-
-#     # Creating the histogram
-#     plt.figure(figsize=(10, 6))
-#     plt.hist(df['quality_score'], bins=20, edgecolor='black', alpha=0.75)
-#     plt.title('Distribution of Quality Scores for Random Greedy Algorithm')
-#     plt.xlabel('Quality Score (K)')
-#     plt.ylabel('Frequency')
-
-#     # Adding the mean and the standard deviation values to the plot
-#     mean = df['quality_score'].mean()
-#     std = df['quality_score'].std()
-#     plt.axvline(mean, color='red', linestyle='dashed', linewidth=1)
-#     plt.text(mean * 1.05, plt.ylim()[1] * 0.9, f'Mean: {mean:.2f}\nStd: {std:.2f}', color='black')
-
-#     # Plotting
-#     file_name = os.path.basename(input_file).replace(".csv", "")
-#     plt.savefig(f'output/quality_scores_histogram_{file_name}.png')
-#     plt.show()
-
-# # Visualize Hill Climber or Simulated Annealing
-# def plot_hillclimb_sim_ann(csv_file="output/hillclimber_results.csv", algorithm='hillclimber'):
-#     """
-#     Plots the course of the quality function K to the iterations.
-
-#     Input:
-#         - csv_file (.csv): iterations in the first row and K in the second
-#         - algorithm (str): choose which algorithm to plot: 'hillclimber' / 'sim_ann'
-
-#     """
-#     iterations = []
-#     scores = []
-#     temperatures = []
-
-#     # read CSV file
-#     with open(csv_file, 'r') as file:
-#         reader = csv.reader(file)
-#         next(reader)  # skip header
-
-#         for row in reader:
-#             iterations.append(int(row[0]))
-#             scores.append(int(row[1]))
-#             if algorithm == "sim_ann":
-#                 temperatures.append(float(row[2]))
-
-#      # create plot
-#     plt.figure(figsize=(10, 6))
-#     plt.plot(iterations, scores, label="Quality Score (K)", color='blue', marker='o')  # Quality scores
-
-#     if algorithm == 'sim_ann':
-#         plt.title("Simulated Annealing: optimization of quality score K")
-#         # plot temperatures as a red line
-#         plt.plot(iterations, temperatures, label="Temperature", color='red', linestyle='--')
-
-#     elif algorithm == 'hillclimber':
-#         plt.title("Hill Climber: optimization of quality score K")
-
-#     plt.xlabel("Iterations")
-#     plt.ylabel("Quality Score (K)")
-#     plt.grid(True)
-#     plt.legend()
-
-#     # Display the plot
-#     plt.show()
-
-
-# if __name__ == "__main__":
-#     plot_hillclimb_sim_ann()
